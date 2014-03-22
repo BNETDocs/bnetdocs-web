b0c669623c419804fd4cf18ab213a17549449d40
Carl Bennett <zalu.cb@gmail.com>
2014-03-22 02:07:01 -0500

    added GitHub reporting to the 500 error page

--- core/BnetDocs.php
+++ core/BnetDocs.php
@@ -14,6 +14,65 @@
       
     }
     
+    public static function fGetCurrentFullURL($sForceURI = '') {
+      
+      if (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443) {
+        $sShareURL = 'https://';
+      } else {
+        $sShareURL = 'http://';
+      }
+      
+      if (\is_string($sForceURI) && !empty($sForceURI)
+       && \substr($sForceURI, 0, 2) == '//') {
+        
+        $sShareURL .= \substr($sForceURI, 2);
+      
+      } else {
+        
+        if (isset($_SERVER['HTTP_HOST'])) {
+          $sShareURL .= $_SERVER['HTTP_HOST'];
+        } else if (isset($_SERVER['SERVER_NAME'])) {
+          $sShareURL .= $_SERVER['SERVER_NAME'];
+        } else {
+          $sShareURL .= 'bnetdocs.org';
+        }
+        
+        if (\is_string($sForceURI) && !empty($sForceURI)
+         && \substr($sForceURI, 0, 1) == '/') {
+          
+          $sShareURL .= $sForceURI;
+          
+        } else {
+          
+          if (isset($_SERVER['REQUEST_URI'])) {
+            $sShareURL .= \parse_url($_SERVER['REQUEST_URI'], \PHP_URL_PATH);
+          } else {
+            $sShareURL .= '/';
+          }
+          
+          if (\is_string($sForceURI) && !empty($sForceURI)
+           && \substr($sForceURI, 0, 1) == '?') {
+            
+            $sQuery = \substr($sForceURI, 1);
+            
+          } else {
+            
+            $sQuery = \http_build_query($_GET);
+            
+          }
+          
+          if (!empty($sQuery)) {
+            $sShareURL .= '?' . $sQuery;
+          }
+          
+        }
+        
+      }
+      
+      return $sShareURL;
+      
+    }
+    
     public static function fInitialize() {
       global $_CONFIG;
       
