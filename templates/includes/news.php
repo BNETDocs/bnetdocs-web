f4f4fa5e10f4651ba1d949999b72e28a9fed22e6
Carl Bennett <zalu.cb@gmail.com>
2014-03-26 02:28:08 -0500

    styling changes to news

--- templates/includes/news.php
+++ templates/includes/news.php
@@ -1,12 +1,19 @@
 <?php
   $sPageTitle = 'News';
-  $sPageAdditionalStyle = 'news_full.css';
+  $sPageAdditionalStyle = BnetDocs::fGetCurrentFullURL('/news_full.css');
   include('./includes/header.php');
 ?>    <div style="position:fixed;left:50%;width:400px;margin-left:-200px;margin-top:20px;margin-bottom:20px;background:#f0f0f0;border-radius:8px;padding:8px;">
 <?php
       foreach ($aNews as $aNewsItem) {
-        echo "      <strong>" . htmlspecialchars($aNewsItem['title'], ENT_QUOTES | ENT_DISALLOWED) . "</strong><br />\n";
-        echo "      <code>" . htmlspecialchars($aNewsItem['content'], ENT_QUOTES | ENT_DISALLOWED) . "</code>\n";
+        echo "      <div class=\"newsitem\">\n";
+        echo "        <div class=\"title\">"
+                   .   "<a href=\"" . BnetDocs::fGetCurrentFullURL('/news/' . urlencode($aNewsItem['id'])) . "\">"
+                   .   htmlspecialchars($aNewsItem['title'], ENT_QUOTES | ENT_DISALLOWED)
+                   .   "</a>"
+                   . "</div>\n";
+        echo "        <div class=\"content\">" . htmlspecialchars($aNewsItem['content'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
+        echo "        <div class=\"creator\">" . htmlspecialchars($aNewsItem['creator'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
+        echo "        <div class=\"post_date\">" . htmlspecialchars($aNewsItem['post_date'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
       }
 ?>    </div>
 <?php include('./includes/footer.php'); ?>
