02ac61ef57e98e5f8e59ca57da34b5093a9629bf
Carl Bennett <zalu.cb@gmail.com>
2014-03-26 02:04:04 -0500

    forgot echo in the testing

--- templates/includes/news.php
+++ templates/includes/news.php
@@ -2,6 +2,6 @@
   $sPageTitle = 'News';
   include('./includes/header.php');
 ?>    <div style="position:fixed;left:50%;width:400px;margin-left:-200px;margin-top:20px;margin-bottom:20px;background:#f0f0f0;border-radius:8px;padding:8px;">
-      <?php str_replace("\n", "<br/>\n      ", json_encode($aNews, JSON_PRETTY_PRINT)); ?>
+      <?php echo str_replace("\n", "<br/>\n      ", json_encode($aNews, JSON_PRETTY_PRINT)); ?>
     </div>
 <?php include('./includes/footer.php'); ?>
