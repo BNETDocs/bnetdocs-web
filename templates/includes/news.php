02506f80017e5a115dd6bc157e03331da886d9c7
Carl Bennett <zalu.cb@gmail.com>
2014-03-26 02:06:24 -0500

    testing a bit more

--- templates/includes/news.php
+++ templates/includes/news.php
@@ -2,6 +2,10 @@
   $sPageTitle = 'News';
   include('./includes/header.php');
 ?>    <div style="position:fixed;left:50%;width:400px;margin-left:-200px;margin-top:20px;margin-bottom:20px;background:#f0f0f0;border-radius:8px;padding:8px;">
-      <?php echo str_replace("\n", "<br/>\n      ", json_encode($aNews, JSON_PRETTY_PRINT)); ?>
-    </div>
+<?php
+      foreach ($aNews as $aNewsItem) {
+        echo "      <strong>" . htmlspecialchars($aNewsItem['title'], ENT_QUOTES | ENT_DISALLOWED) . "</strong><br />\n";
+        echo "      <code>" . htmlspecialchars($aNewsItem['content'], ENT_QUOTES | ENT_DISALLOWED) . "</code>\n";
+      }
+?>    </div>
 <?php include('./includes/footer.php'); ?>
