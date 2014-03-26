c531d174d9cb2585df00b26c62ca2da6b6e4037c
Carl Bennett <zalu.cb@gmail.com>
2014-03-26 01:48:21 -0500

    testing database access

--- templates/includes/news.php
+++ templates/includes/news.php
@@ -1,10 +1,7 @@
 <?php
   $sPageTitle = 'News';
   include('./includes/header.php');
-?>    <div id="container">
-      <div class="title">News</div>
-      <div class="message">
-        There is no news at the moment. Check back later.
-      </div>
+?>    <div style="position:fixed;left:50%;width:400px;margin-left:-200px;background:#f0f0f0;">
+      <?php str_replace("\n", "<br/>\n      ", json_encode($aNews, JSON_PRETTY_PRINT)); ?>
     </div>
 <?php include('./includes/footer.php'); ?>
--- templates/pages/news.php
+++ templates/pages/news.php
@@ -1,4 +1,22 @@
 <?php
+
+  $oResult = BnetDocs::$oDB->fQuery('SELECT n.id, u.username, n.date_posted, n.edit_count, n.edit_date, n.title, n.content FROM news n LEFT JOIN users u ON n.creator_uid = u.id ORDER BY n.date_posted DESC LIMIT 0,3;');
+
+  if (!$oResult || !$oResult instanceof MySQLResult) {
+    $aNews = array();
+  } else {
+    $aNews = array(
+      0 => array(
+        'id' => 0,
+        'username' => 'hardcoded-test',
+        'date_posted' => date('Y-m-d H:i:s T'),
+        'edit_count' => 1,
+        'edit_date' => date('Y-m-d H:i:s T'),
+        'title' => 'hardcoded test title',
+        'content' => 'this is a hardcoded test',
+      ),
+    );
+  }
   
   ob_start('ob_gzhandler');
   include_once('./includes/news.php');
