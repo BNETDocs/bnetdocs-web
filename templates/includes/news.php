ea7e55f30f97752d582b81be3bd2b7a3a7139cdb
Jailout2000 <zalu.cb@gmail.com>
2014-03-29 22:52:26 -0500

    added content filters and fixed unclosed div tag

--- templates/includes/news.php
+++ templates/includes/news.php
@@ -2,7 +2,7 @@
   $sPageTitle = 'News';
   $sPageAdditionalStyle = BnetDocs::fGetCurrentFullURL('/news_full.css');
   include('./includes/header.php');
-?>    <div style="position:fixed;left:50%;width:400px;margin-left:-200px;margin-top:20px;margin-bottom:20px;background:#f0f0f0;border-radius:8px;padding:8px;">
+?>    <div style="margin-top:20px;margin-bottom:20px;background:#f0f0f0;border-radius:8px;padding:8px;">
 <?php
       foreach ($aNews as $aNewsItem) {
         echo "      <div class=\"newsitem\">\n";
@@ -11,9 +11,10 @@
                    .   htmlspecialchars($aNewsItem['title'], ENT_QUOTES | ENT_DISALLOWED)
                    .   "</a>"
                    . "</div>\n";
-        echo "        <div class=\"content\">" . htmlspecialchars($aNewsItem['content'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
+        echo "        <div class=\"content\">" . ContentFilter::fFilterBBCode(ContentFilter::fFilterHTML($aNewsItem['content'])) . "</div>\n";
         echo "        <div class=\"creator\">" . htmlspecialchars($aNewsItem['creator'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
         echo "        <div class=\"post_date\">" . htmlspecialchars($aNewsItem['post_date'], ENT_QUOTES | ENT_DISALLOWED) . "</div>\n";
+        echo "      </div>\n";
       }
 ?>    </div>
 <?php include('./includes/footer.php'); ?>
