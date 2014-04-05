20f68a727f2a44312d58c3bbac4df64407cc7b27
Jailout2000 <zalu.cb@gmail.com>
2014-04-05 00:15:50 -0500

    added title attributes to the login page

--- templates/includes/user/login.php
+++ templates/includes/user/login.php
@@ -14,11 +14,11 @@
     } else {
       echo "        <p>Submit your username and password below to log in.</p>\n";
     }
-    echo "          <label for=\"username\">Username:</label>\n";
-    echo "          <input id=\"username\" name=\"username\" type=\"text\" tabindex=\"1\"" . (empty($sPasswordResetFailed) && !$bPasswordResetSuccess ? " autofocus=\"autofocus\"" : "") . " value=\"" . $sUsername . "\" />\n";
-    echo "          <label for=\"password\">Password:</label>\n";
-    echo "          <input id=\"password\" name=\"password\" type=\"password\" tabindex=\"2\" value=\"\" />\n";
-    echo "          <input id=\"login\" type=\"submit\" tabindex=\"3\" value=\"Log In\" />\n";
+    echo "          <label for=\"username\" title=\"Enter the username you used when you created the account.\">Username:</label>\n";
+    echo "          <input id=\"username\" name=\"username\" type=\"text\" tabindex=\"1\"" . (empty($sPasswordResetFailed) && !$bPasswordResetSuccess ? " autofocus=\"autofocus\"" : "") . " title=\"Enter the username you used when you created the account.\" value=\"" . htmlspecialchars($sUsername, ENT_XML1, "UTF-8") . "\" />\n";
+    echo "          <label for=\"password\" title=\"Enter the password you use with the account.\">Password:</label>\n";
+    echo "          <input id=\"password\" name=\"password\" type=\"password\" tabindex=\"2\" title=\"Enter the password you use with the account.\" value=\"\" />\n";
+    echo "          <input id=\"login\" type=\"submit\" tabindex=\"3\" title=\"Click to log in to your account.\" value=\"Log In\" />\n";
     echo "        </div>\n";
     echo "      </form>\n";
     $sPasswordResetFormClass = "";
@@ -35,16 +35,16 @@
     } else {
       echo "        <p>Submit your username below and an email will be sent.</p>\n";
     }
-    echo "          <label for=\"username_2\">Username:</label>\n";
-    echo "          <input id=\"username_2\" name=\"username\" type=\"text\" tabindex=\"4\"" . (!empty($sPasswordResetFailed) || $bPasswordResetSuccess ? " autofocus=\"autofocus\"" : "") . " value=\"" . $sUsername . "\" />\n";
-    echo "          <input id=\"reset_password\" type=\"submit\" tabindex=\"5\" value=\"Reset Password\" />\n";
+    echo "          <label for=\"username_2\" title=\"Enter the username you used when you created the account.\">Username:</label>\n";
+    echo "          <input id=\"username_2\" name=\"username\" type=\"text\" tabindex=\"4\"" . (!empty($sPasswordResetFailed) || $bPasswordResetSuccess ? " autofocus=\"autofocus\"" : "") . " title=\"Enter the username you used when you created the account.\" value=\"" . htmlspecialchars($sUsername, ENT_XML1, "UTF-8") . "\" />\n";
+    echo "          <input id=\"reset_password\" type=\"submit\" tabindex=\"5\" title=\"Click to send an email with a reset password link.\" value=\"Reset Password\" />\n";
     echo "        </div>\n";
     echo "      </form>\n";
     echo "      <form method=\"GET\" action=\"/user/register\">\n";
     echo "        <div class=\"title\">Create Account</div>\n";
     echo "        <div class=\"content\" id=\"register_form\">\n";
-    echo "          <input type=\"hidden\" name=\"username\" value=\"" . $sUsername . "\" />\n";
-    echo "          <input id=\"register\" type=\"submit\" tabindex=\"6\" value=\"Go to Registration Form\" />\n";
+    echo "          <input type=\"hidden\" name=\"username\" value=\"" . htmlspecialchars($sUsername, ENT_XML1, "UTF-8") . "\" />\n";
+    echo "          <input id=\"register\" type=\"submit\" tabindex=\"6\" title=\"Click to be taken to the account registration form.\" value=\"Go to Registration Form\" />\n";
     echo "        </div>\n";
     echo "      </form>\n";
   include('./includes/footer.php');
