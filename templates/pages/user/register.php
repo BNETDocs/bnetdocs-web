abd44771f9678b325914eeb637cb1e2760d3ffb1
Jailout2000 <zalu.cb@gmail.com>
2014-04-04 23:27:21 -0500

    fix error when there is no error

--- templates/pages/user/register.php
+++ templates/pages/user/register.php
@@ -11,40 +11,44 @@
   $sEmailOne    = (isset($aQuery['email_1'])      ? $aQuery['email_1']      : '');
   $sEmailTwo    = (isset($aQuery['email_2'])      ? $aQuery['email_2']      : '');
   
-  if (strtolower($sEmailOne) != strtolower($sEmailTwo)) {
-    $sUserRegisterFailed = "The two email addresses do not match.";
-  } else if ($sPasswordOne != $sPasswordTwo) {
-    $sUserRegisterFailed = "The two passwords do not match.";
-  } else if (strlen($sPasswordOne) < User::PASSWORD_LENGTH_MINIMUM) {
-    $sUserRegisterFailed = "Your password must be at least " . User::PASSWORD_LENGTH_MINIMUM . " characters.";
-  } else if (strlen($sPasswordOne) > User::PASSWORD_LENGTH_MAXIMUM) {
-    $sUserRegisterFailed = "Your password must be at most " . User::PASSWORD_LENGTH_MAXIMUM . " characters.";
-  } else if (User::PASSWORD_CANNOT_CONTAIN_USERNAME && stripos($sPasswordOne, $sUsername) !== false) {
-    $sUserRegisterFailed = "Your password cannot contain your username.";
-  } else if (User::PASSWORD_CANNOT_CONTAIN_DISPLAYNAME && stripos($sPasswordOne, $sDisplayName) !== false) {
-    $sUserRegisterFailed = "Your password cannot contain your display name.";
-  } else if (User::PASSWORD_CANNOT_CONTAIN_EMAIL && stripos($sPasswordOne, $sEmailOne) !== false) {
-    $sUserRegisterFailed = "Your password cannot contain your email address.";
-  } else if (User::PASSWORD_REQUIRES_UPPERCASE_LETTERS && !preg_match('/[A-Z]/', $sPasswordOne)) {
-    $sUserRegisterFailed = "Your password must use at least one uppercase letter.";
-  } else if (User::PASSWORD_REQUIRES_LOWERCASE_LETTERS && !preg_match('/[a-z]/', $sPasswordOne)) {
-    $sUserRegisterFailed = "Your password must use at least one lowercase letter.";
-  } else if (User::PASSWORD_REQUIRES_NUMBERS && !preg_match('/[0-9]/', $sPasswordOne)) {
-    $sUserRegisterFailed = "Your password must use at least one numeric character.";
-  } else if (User::PASSWORD_REQUIRES_SYMBOLS && !preg_match("/['\":;^£$%&*()}{\\[\\]@#~\\?><>,.\\/|=_+¬\\-]/", $sPasswordOne)) {
-    $sUserRegisterFailed = "Your password must use at least one symbol.";
+  if (!$aQuery) {
+    $sUserRegisterFailed = "";
   } else {
-    
-    $oUser = User::fFindUserByUsername($sUsername);
-    
-    if ($oUser) {
-      $sUserRegisterFailed = "That username is already registered. Pick a unique name.";
+    if (strtolower($sEmailOne) != strtolower($sEmailTwo)) {
+      $sUserRegisterFailed = "The two email addresses do not match.";
+    } else if ($sPasswordOne != $sPasswordTwo) {
+      $sUserRegisterFailed = "The two passwords do not match.";
+    } else if (strlen($sPasswordOne) < User::PASSWORD_LENGTH_MINIMUM) {
+      $sUserRegisterFailed = "Your password must be at least " . User::PASSWORD_LENGTH_MINIMUM . " characters.";
+    } else if (strlen($sPasswordOne) > User::PASSWORD_LENGTH_MAXIMUM) {
+      $sUserRegisterFailed = "Your password must be at most " . User::PASSWORD_LENGTH_MAXIMUM . " characters.";
+    } else if (User::PASSWORD_CANNOT_CONTAIN_USERNAME && stripos($sPasswordOne, $sUsername) !== false) {
+      $sUserRegisterFailed = "Your password cannot contain your username.";
+    } else if (User::PASSWORD_CANNOT_CONTAIN_DISPLAYNAME && stripos($sPasswordOne, $sDisplayName) !== false) {
+      $sUserRegisterFailed = "Your password cannot contain your display name.";
+    } else if (User::PASSWORD_CANNOT_CONTAIN_EMAIL && stripos($sPasswordOne, $sEmailOne) !== false) {
+      $sUserRegisterFailed = "Your password cannot contain your email address.";
+    } else if (User::PASSWORD_REQUIRES_UPPERCASE_LETTERS && !preg_match('/[A-Z]/', $sPasswordOne)) {
+      $sUserRegisterFailed = "Your password must use at least one uppercase letter.";
+    } else if (User::PASSWORD_REQUIRES_LOWERCASE_LETTERS && !preg_match('/[a-z]/', $sPasswordOne)) {
+      $sUserRegisterFailed = "Your password must use at least one lowercase letter.";
+    } else if (User::PASSWORD_REQUIRES_NUMBERS && !preg_match('/[0-9]/', $sPasswordOne)) {
+      $sUserRegisterFailed = "Your password must use at least one numeric character.";
+    } else if (User::PASSWORD_REQUIRES_SYMBOLS && !preg_match("/['\":;^£$%&*()}{\\[\\]@#~\\?><>,.\\/|=_+¬\\-]/", $sPasswordOne)) {
+      $sUserRegisterFailed = "Your password must use at least one symbol.";
     } else {
-      $sUserRegisterFailed = "";
+      
+      $oUser = User::fFindUserByUsername($sUsername);
+      
+      if ($oUser) {
+        $sUserRegisterFailed = "That username is already registered. Pick a unique name.";
+      } else {
+        $sUserRegisterFailed = "";
+      }
+      
+      //} else if (!Email::fSendWelcome($oUser)) {
+      //  $sUserRegisterFailed = "Failed to send a welcome email to your new user. You will need to do a password reset to access your account.";
     }
-    
-    //} else if (!Email::fSendWelcome($oUser)) {
-    //  $sUserRegisterFailed = "Failed to send a welcome email to your new user. You will need to do a password reset to access your account.";
   }
   
   ob_start('ob_gzhandler');
