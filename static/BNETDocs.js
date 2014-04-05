1fd6b4f9747f0557be6fef99979d87b571e92967
Jailout2000 <zalu.cb@gmail.com>
2014-04-05 03:41:38 -0500

    fixed ajax framework

--- static/BNETDocs.js
+++ static/BNETDocs.js
@@ -21,6 +21,22 @@ function BNETDocs() {
     }
   }
   
+  this.fSetExtraStyle = function(styleHref) {
+    var link_elements = document.getElementsByTagName('link');
+    var link_found = false;
+    for (var id in link_elements) {
+      if (link_elements[id].href == styleHref) {
+        link_found = true;
+        break;
+      }
+    }
+    if (!link_found)
+      document.getElementsByTagName('head')[0].innerHTML
+        += '<link rel="stylesheet" href="'
+         + styleHref
+         + '" type="text/css" media="all" />';
+  }
+  
   this.fPageLoadAjax = function(href) {
     var xhr, url;
     if (window.XMLHttpRequest) {
@@ -33,23 +49,15 @@ function BNETDocs() {
         try {
           if (this.status != 200) throw new Error;
           var content_element = document.getElementById('content');
+          var pageContent     = this.response;
           var pageTitle       = this.getResponseHeader('X-Page-Title');
           var pageExtraStyle  = this.getResponseHeader('X-Page-Extra-Style');
-          link_elements = document.getElementsByTagName('link');
-          link_found = false;
-          for (var id in link_elements) {
-            if (link_elements[id].href == pageExtraStyle) {
-              link_found = true;
-              break;
-            }
-          }
-          if (!link_found)
-            document.getElementsByTagName('head')[0].innerHTML
-              += '<link rel="stylesheet" href="'
-               + pageExtraStyle
-               + '" type="text/css" media="all" />';
-          content_element.innerHTML = this.response;
-          history.pushState(content_element, pageTitle, href);
+          self.fSetExtraStyle(pageExtraStyle);
+          content_element.innerHTML = pageContent;
+          history.pushState({
+            'extraStyle': pageExtraStyle,
+            'content': pageContent
+          }, pageTitle, href);
         } catch (e) {
           window.location = href;
         }
@@ -66,13 +74,19 @@ function BNETDocs() {
   }
   
   window.onload = function() {
-    history.replaceState(document.getElementById('content'));
     self.fOverrideNavigationAnchors();
   }
   
   window.onpopstate = function(event) {
-    if (typeof event.state == 'string') {
-      document.getElementById('content') = event.state;
+    var obj = document.getElementById('content');
+    if (!event.state) {
+      history.replaceState({
+        'extraStyle': '',
+        'content': obj.innerHTML
+      });
+    } else {
+      self.fSetExtraStyle(event.state.extraStyle);
+      obj.innerHTML = event.state.content;
     }
   }
   
