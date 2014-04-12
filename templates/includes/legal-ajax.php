cfc26cc29a61748843aafe59f281ac6b55d5ee07
Jailout2000 <zalu.cb@gmail.com>
2014-04-12 01:52:06 -0500

    added terms of service page so it doesn't require 404

---
+++ templates/includes/legal-ajax.php
@@ -0,0 +1,6 @@
+<?php
+  $sPageTitle = 'Terms of Service - BNETDocs';
+  //$sPageAdditionalStyle = BNETDocs::fGetCurrentFullURL('/news_item.css', true);
+  $oContext->fSetResponseHeader('X-Page-Title', $sPageTitle);
+  //$oContext->fSetResponseHeader('X-Page-Extra-Style', $sPageAdditionalStyle);
+  
---
+++ templates/includes/legal.php
@@ -0,0 +1,7 @@
+<?php
+  $sPageTitle = 'Terms of Service';
+  //$sPageAdditionalStyle = BNETDocs::fGetCurrentFullURL('/news_item.css', true);
+  include('./includes/header.php');
+  include('./includes/legal-ajax.php');
+  include('./includes/footer.php');
+  
