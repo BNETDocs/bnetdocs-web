7ef7cabe5fd88ce155ca5f3a36394e56b5dedf93
Jailout2000 <zalu.cb@gmail.com>
2014-04-12 19:30:34 -0500

    added legal jargon to project

--- templates/includes/legal.php
+++ templates/includes/legal.php
@@ -1,6 +1,6 @@
 <?php
   $sPageTitle = 'Terms of Service';
-  //$sPageAdditionalStyle = BNETDocs::fGetCurrentFullURL('/news_item.css', true);
+  $sPageAdditionalStyle = BNETDocs::fGetCurrentFullURL('/legal_page.css', true);
   include('./includes/header.php');
   include('./includes/legal-ajax.php');
   include('./includes/footer.php');
