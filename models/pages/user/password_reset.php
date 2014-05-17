3490ae6ad6ab5edf6f39679ef23c7801457fb778
Jailout2000 <zalu.cb@gmail.com>
2014-05-16 23:16:26 -0500

    fixed error when password_hash is NULL from database, users would get stacktrace because hash wasn't a string

--- models/pages/user/password_reset.php
+++ models/pages/user/password_reset.php
@@ -60,10 +60,10 @@
           $sFocusField = "password_1";
           $mResult     = $mCheckForm;
         } else {
-          BNETDocs::$oUserSession->fSetUserObjectByObject($oUser);
-          BNETDocs::$oUserSession->fSetSessionCookie();
           $sFocusField = "username";
           if (!($oUser->fSetVerifiedDate(date('Y-m-d H:i:s')) && $oUser->fSetPassword($sPasswordOne))) {
+            BNETDocs::$oUserSession->fSetUserObjectByObject($oUser);
+            BNETDocs::$oUserSession->fSetSessionCookie();
             BNETDocs::$oLogger->fLogEvent('user_pw_reset', $oContext->fGetRequestIPAddress(), $oUser->fGetUId(), array('success' => false));
             $mResult = "A server error occurred while trying to save your account changes into our database.";
           } else {
