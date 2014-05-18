d4456f2e9b9b2e9dcd25bd61f00b6187ec8ea58f
Jailout2000 <zalu.cb@gmail.com>
2014-05-18 04:49:59 -0500

    changes to edit profile page

--- models/includes/user/profile/edit-ajax.php
+++ models/includes/user/profile/edit-ajax.php
@@ -7,9 +7,9 @@
   echo "      <form method=\"POST\" action=\"/user/profile/edit\">\n";
   echo "        <div class=\"title\">Edit Profile</div>\n";
   echo "        <div class=\"content\">\n";
-  echo "          <p>Here you can edit your user profile details.</p>\n";
+  echo "          <p>You can edit your user profile details using this form.</p>\n";
   echo "          <input type=\"hidden\" name=\"csrf\" value=\"" . htmlspecialchars(AntiCSRF::fGetToken(), ENT_XML1, "UTF-8") . "\" />\n";
-  echo "          <label for=\"display_name\" title=\"Enter an alternate name than your username you wish to use.\">Username:</label>\n";
+  echo "          <label for=\"display_name\" title=\"Enter an alternate name than your username you wish to use.\">Display Name:</label>\n";
   echo "          <input id=\"display_name\" name=\"display_name\" type=\"text\" tabindex=\"1\"" . ($sFocusField == "display_name" ? " autofocus=\"autofocus\"" : "") . " required=\"required\" title=\"Enter an alternate name than your username you wish to use.\" value=\"" . htmlspecialchars($sDisplayName, ENT_XML1, "UTF-8") . "\" />\n";
   echo "        </div>\n";
   echo "      </form>\n";
