6f6776df335ebcd7743d25e01da386ca4aae4200
Jailout2000 <zalu.cb@gmail.com>
2014-05-18 03:55:55 -0500

    moved top news contributors up above the packet contributors

--- models/includes/credits-ajax.php
+++ models/includes/credits-ajax.php
@@ -34,29 +34,6 @@
   echo "          </section>\n";
   echo "        </article>\n";
   echo "        <article class=\"statistic\">\n";
-  echo "          <h1>Top Packet Contributors</h1>\n";
-  echo "          <section>\n";
-  echo "            <p>These users have added the most packets to the website.</p>\n";
-  if (!$aTopPacketContributors) {
-    echo "            <p class=\"none\">There have been no contributors at this time.</p>\n";  
-  } else {
-    echo "            <ol>\n";
-    foreach ($aTopPacketContributors as $aContributor) {
-      $sUsername    = "";
-      $sDisplayName = "Anonymous";
-      if ($aContributor[1] instanceof User) {
-        $sUsername    = $aContributor[1]->fGetUsername();
-        $sDisplayName = $aContributor[1]->fGetDisplayName();
-      }
-      $sName    = ContentFilter::fFilterHTML((empty($sDisplayName) ? $sUsername : $sDisplayName));
-      $sPackets = number_format($aContributor[0]) . " packet" . ($aContributor[0] != 1 ? "s" : "");
-      echo "              <li><strong>" . $sName . "</strong> with <strong>" . $sPackets . "</strong>.</li>\n";
-    }
-    echo "            </ol>\n";
-  }
-  echo "          </section>\n";
-  echo "        </article>\n";
-  echo "        <article class=\"statistic\">\n";
   echo "          <h1>Top News Contributors</h1>\n";
   echo "          <section>\n";
   echo "            <p>These users have added the most news to the website.</p>\n";
@@ -74,6 +51,29 @@
       $sName      = ContentFilter::fFilterHTML((empty($sDisplayName) ? $sUsername : $sDisplayName));
       $sNewsPosts = number_format($aContributor[0]) . " news post" . ($aContributor[0] != 1 ? "s" : "");
       echo "              <li><strong>" . $sName . "</strong> with <strong>" . $sNewsPosts . "</strong>.</li>\n";
+    }
+    echo "            </ol>\n";
+  }
+  echo "          </section>\n";
+  echo "        </article>\n";
+  echo "        <article class=\"statistic\">\n";
+  echo "          <h1>Top Packet Contributors</h1>\n";
+  echo "          <section>\n";
+  echo "            <p>These users have added the most packets to the website.</p>\n";
+  if (!$aTopPacketContributors) {
+    echo "            <p class=\"none\">There have been no contributors at this time.</p>\n";  
+  } else {
+    echo "            <ol>\n";
+    foreach ($aTopPacketContributors as $aContributor) {
+      $sUsername    = "";
+      $sDisplayName = "Anonymous";
+      if ($aContributor[1] instanceof User) {
+        $sUsername    = $aContributor[1]->fGetUsername();
+        $sDisplayName = $aContributor[1]->fGetDisplayName();
+      }
+      $sName    = ContentFilter::fFilterHTML((empty($sDisplayName) ? $sUsername : $sDisplayName));
+      $sPackets = number_format($aContributor[0]) . " packet" . ($aContributor[0] != 1 ? "s" : "");
+      echo "              <li><strong>" . $sName . "</strong> with <strong>" . $sPackets . "</strong>.</li>\n";
     }
     echo "            </ol>\n";
   }
