bb748ce9f807c765dec35ddc022ff204b0b7e8f7
Carl Bennett <carlbennett@users.noreply.github.com>
2014-07-27 21:23:46 -0500

    remove useless files

--- encryption.php
+++
@@ -1,9 +0,0 @@
-<?
-	$str = 'This is a string.';
-	echo 'Before encoding: '.$str.'<br>';
-	$str = base64_encode($str);
-	echo 'After encoding: '.$str.'<br>';
-	$str = base64_decode($str);
-	echo 'After decoding using base64_decode: '.$str.'<br>';
-	echo 'Done.';
-?>
--- test.php
+++
@@ -1,3 +0,0 @@
-<?
-	echo $_REQUEST['direction'];
-?>
