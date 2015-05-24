4d8e63a8d7dfd39d3e5b027fc4bb2b606dfb9ecf
Carl Bennett <carlbennett@users.noreply.github.com>
2015-05-24 12:17:59 +0000

    Add a sample database and exclude it from rsync.

---
+++ database.sample.sql
@@ -0,0 +1,12 @@
+-- BNETDocs: Phoenix
+-- Sample Database
+-- 2015-05-24 07:14 CDT
+--
+
+-- Begin sample.
+
+DROP DATABASE IF EXISTS `bnetdocs-phoenix`;
+
+CREATE DATABASE `bnetdocs-phoenix` CHARSET `utf8` COLLATE `utf8_unicode_ci`;
+
+-- End of sample.
