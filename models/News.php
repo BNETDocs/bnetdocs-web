6e1be5c28059b47c9b40d61d0789b04eb5303f00
Carl Bennett <carlbennett@users.noreply.github.com>
2015-05-26 03:00:26 +0000

    Move sample news post from template to model.

--- models/News.php
+++ models/News.php
@@ -11,7 +11,15 @@ class News extends Model {
   public $news_posts;
 
   public function __construct() {
-    $this->news_posts = [];
+    $this->news_posts = [
+      0 => [
+        "id" => 0,
+        "author" => "Carl Bennett",
+        "timestamp_published" => new \DateTime("2015-05-25 21:53:00 CDT"),
+        "title" => "Work in progress",
+        "content" => "I've been giving life back into BNETDocs: Phoenix recently. There's been lots of changes to the code repository and restructuring it. There's been lots of new designs and paradigms put in place that are better than the previous Phoenix from last year. More news coming soon."
+      ]
+    ];
   }
 
 }
