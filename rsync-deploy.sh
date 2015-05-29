76a4cd1a204e00df2dee810bcf3fa456636a0fb2
Carl Bennett <carlbennett@users.noreply.github.com>
2015-05-29 02:33:48 +0000

    Update deploy script

--- rsync-deploy.sh
+++ rsync-deploy.sh
@@ -8,12 +8,13 @@ function check_bash_version () {
 }
 
 function deploy_project () {
-  printf "[%s] deploying...\n" "$(date)"
   cd "$(git rev-parse --show-toplevel)"
+  printf "[%s] deploying to web1.localdomain...\n" "$(date)"
   rsync -avzc --delete --delete-excluded --delete-after \
     --progress --exclude-from="./rsync-exclude.txt" \
     "./" web1.localdomain:"/home/nginx/bnetdocs-dev" ||
     return $?
+  printf "[%s] deploying to web2.localdomain...\n" "$(date)"
   rsync -avzc --delete --delete-excluded --delete-after \
     --progress --exclude-from="./rsync-exclude.txt" \
     "./" web2.localdomain:"/home/nginx/bnetdocs-dev" ||
