ae02e1131ebcfbee35004fa3ed2a06c4694dbb25
Carl Bennett <carlbennett@users.noreply.github.com>
2016-03-16 07:32:23 +0000

    Refactor deploy script

--- rsync-deploy.sh
+++ rsync-deploy.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-function check_bash_version () {
-  if [[ "${BASH_VERSION}" != "4."* ]]; then
-    printf "[%s] bash is too old\n" "$(date)" >&2
-    exit 1
-  fi
-}
+if [ -z "${SOURCE_DIRECTORY}" ]; then
+  SOURCE_DIRECTORY="$(git rev-parse --show-toplevel)/"
+fi
+if [ -z "${TARGET_DIRECTORY}" ]; then
+  TARGET_DIRECTORY="/home/nginx/bnetdocs-dev"
+fi
 
-function deploy_project () {
-  cd "$(git rev-parse --show-toplevel)"
-  printf "[%s] deploying to lstn.carlbennett.me...\n" "$(date)"
-  rsync -avzc --delete --delete-excluded --delete-after \
-    --progress --exclude-from="./rsync-exclude.txt" \
-    --chown=nginx:www-data --rsync-path="sudo rsync" \
-    "./" lstn.carlbennett.me:"/home/nginx/bnetdocs-dev" ||
-    return $?
-}
+DEPLOY_TARGET="$1"
+if [ -z "${DEPLOY_TARGET}" ]; then
+  DEPLOY_TARGET="$(cat ${SOURCE_DIRECTORY}/.rsync-target 2>/dev/null)"
+fi
+if [ -z "${DEPLOY_TARGET}" ]; then
+  read -p "Enter the server to deploy to: " DEPLOY_TARGET
+fi
+if [ -z "${DEPLOY_TARGET}" ]; then
+  printf "Deploy target not provided, aborting...\n" 1>&2
+  exit 1
+fi
+echo "${DEPLOY_TARGET}" > ${SOURCE_DIRECTORY}/.rsync-target
 
-function main () {
-  printf "[%s] initializing...\n" "$(date)"
-  check_bash_version
-  deploy_project &&
-    printf "[%s] success\n" "$(date)" ||
-    printf "[%s] failed to deploy project\n" "$(date)"
-}
+set -e
 
-main
+printf "[1/1] Syncing to deploy target...\n"
+rsync -avzc --delete --delete-excluded --delete-after --progress \
+  --exclude-from="${SOURCE_DIRECTORY}/rsync-exclude.txt" \
+  --chown=nginx:www-data --rsync-path="sudo rsync" \
+  "${SOURCE_DIRECTORY}" \
+  ${DEPLOY_TARGET}:"${TARGET_DIRECTORY}"
+
+printf "Operation complete!\n"
