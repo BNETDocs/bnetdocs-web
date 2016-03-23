d6aacae994d183f1f896607ef03b8c756f04b5ff
Carl Bennett <carlbennett@users.noreply.github.com>
2016-03-23 04:50:23 +0000

    Refactor document sitemap

--- controllers/Document/Sitemap.php
+++ controllers/Document/Sitemap.php
@@ -6,11 +6,14 @@ use \BNETDocs\Libraries\Common;
 use \BNETDocs\Libraries\Controller;
 use \BNETDocs\Libraries\Document;
 use \BNETDocs\Libraries\Exceptions\UnspecifiedViewException;
+use \BNETDocs\Libraries\Gravatar;
 use \BNETDocs\Libraries\Router;
 use \BNETDocs\Libraries\UserSession;
 use \BNETDocs\Models\Document\Sitemap as DocumentSitemapModel;
 use \BNETDocs\Views\Document\SitemapHtml as DocumentSitemapHtmlView;
 use \BNETDocs\Views\Document\SitemapJSON as DocumentSitemapJSONView;
+use \DateTime;
+use \DateTimeZone;
 
 class Sitemap extends Controller {
 
@@ -26,8 +29,64 @@ class Sitemap extends Controller {
         throw new UnspecifiedViewException();
     }
     $model = new DocumentSitemapModel();
-    $model->documents    = Document::getAllDocuments();
-    $model->user_session = UserSession::load($router);
+    
+    $model->documents     = Document::getAllDocuments();
+    $model->user_session  = UserSession::load($router);
+
+    // Alphabetically sort the documents for HTML
+    if ($view instanceof DocumentSitemapHtmlView && $model->documents) {
+      usort($model->documents, function($a, $b){
+        $a1 = $a->getTitle();
+        $b1 = $b->getTitle();
+        if ($a1 == $b1) return 0;
+        return ($a1 < $b1 ? -1 : 1);
+      });
+    }
+
+    // Remove documents that are not published
+    if ($model->documents) {
+      $i = count($model->documents) - 1;
+      while ($i >= 0) {
+        if (!($model->documents[$i]->getOptionsBitmask()
+          & Document::OPTION_PUBLISHED)) {
+          unset($model->documents[$i]);
+        }
+        --$i;
+      }
+    }
+
+    // Objectify for JSON
+    if ($view instanceof DocumentSitemapJSONView) {
+      $model->timestamp = new DateTime("now", new DateTimeZone("UTC"));
+      $documents = [];
+      foreach ($model->documents as $document) {
+        $user = $document->getUser();
+        $documents[] = [
+          "content"          => $document->getContent(false),
+          "created_datetime" => self::renderDateTime($document->getCreatedDateTime()),
+          "edited_count"     => $document->getEditedCount(),
+          "edited_datetime"  => self::renderDateTime($document->getEditedDateTime()),
+          "id"               => $document->getId(),
+          "options_bitmask"  => $document->getOptionsBitmask(),
+          "title"            => $document->getTitle(),
+          "user"             => [
+            "avatar_url" => "https:"
+              . (new Gravatar($user->getEmail()))->getUrl(null, "identicon"),
+            "id"     => $user->getId(),
+            "name"   => $user->getName(),
+            "url"    => Common::relativeUrlToAbsolute(
+              "/user/" . $user->getId() . "/"
+              . Common::sanitizeForUrl($user->getName())
+            )
+          ]
+        ];
+      }
+      $model->documents = $documents;
+    }
+
+    // Post-filter summary of documents
+    $model->sum_documents = count($model->documents);
+
     ob_start();
     $view->render($model);
     $router->setResponseCode(200);
@@ -35,6 +94,11 @@ class Sitemap extends Controller {
     $router->setResponseHeader("Content-Type", $view->getMimeType());
     $router->setResponseContent(ob_get_contents());
     ob_end_clean();
+  }
+  
+  protected static function renderDateTime($obj) {
+    if (!$obj instanceof DateTime) return $obj;
+    return $obj->format("r");
   }
 
 }
