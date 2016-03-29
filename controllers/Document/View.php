75d53e7c7c6bb8b7d2ccaf43286a5e4b777243ef
Carl Bennett <carlbennett@users.noreply.github.com>
2016-03-29 03:19:05 +0000

    Implement not found page for documents

--- controllers/Document/View.php
+++ controllers/Document/View.php
@@ -5,6 +5,7 @@ namespace BNETDocs\Controllers\Document;
 use \BNETDocs\Libraries\Common;
 use \BNETDocs\Libraries\Controller;
 use \BNETDocs\Libraries\Document;
+use \BNETDocs\Libraries\Exceptions\DocumentNotFoundException;
 use \BNETDocs\Libraries\Exceptions\UnspecifiedViewException;
 use \BNETDocs\Libraries\Router;
 use \BNETDocs\Libraries\UserSession;
@@ -35,11 +36,16 @@ class View extends Controller {
         throw new UnspecifiedViewException();
     }
     $model = new DocumentViewModel();
-    $model->document     = new Document($this->document_id);
+    $model->document_id  = $this->document_id;
+    try {
+      $model->document   = new Document($this->document_id);
+    } catch (DocumentNotFoundException $e) {
+      $model->document   = null;
+    }
     $model->user_session = UserSession::load($router);
     ob_start();
     $view->render($model);
-    $router->setResponseCode(200);
+    $router->setResponseCode(($model->document ? 200 : 404));
     $router->setResponseTTL(0);
     $router->setResponseHeader("Content-Type", $view->getMimeType());
     $router->setResponseContent(ob_get_contents());
