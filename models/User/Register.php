258f9041f13ac9aa7a6db93c9a66a3a7bc2c23d1
Carl Bennett <carlbennett@users.noreply.github.com>
2016-04-28 05:50:20 +0000

    Fix E_NOTICE error with User/Register model

--- models/User/Register.php
+++ models/User/Register.php
@@ -6,10 +6,14 @@ use \BNETDocs\Libraries\Model;
 
 class Register extends Model {
 
+  public $email;
+  public $username;
   public $user_session;
 
   public function __construct() {
     parent::__construct();
+    $this->email        = null;
+    $this->username     = null;
     $this->user_session = null;
   }
 
