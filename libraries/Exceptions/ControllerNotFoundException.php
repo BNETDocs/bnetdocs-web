eaa2dabdc032671e6845036b79ef3965aa699d4f
Carl Bennett <carlbennett@users.noreply.github.com>
2016-04-28 05:52:46 +0000

    Fix E_NOTICE with ControllerNotFoundException

--- libraries/Exceptions/ControllerNotFoundException.php
+++ libraries/Exceptions/ControllerNotFoundException.php
@@ -10,7 +10,7 @@ class ControllerNotFoundException extends BNETDocsException {
 
   public function __construct($controllerName, Exception &$prev_ex = null) {
     parent::__construct("Unable to find a suitable controller given the path", 2, $prev_ex);
-    Logger::logMetric("controllerName", $className);
+    Logger::logMetric("controllerName", $controllerName);
     $this->httpResponseCode = 404;
   }
 
