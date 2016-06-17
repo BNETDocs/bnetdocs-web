0b4486b62a2b062c0a1605f2f2b7f174891e8827
Carl Bennett <carlbennett@users.noreply.github.com>
2016-06-17 14:39:27 +0000

    Padding

--- libraries/Recaptcha.php
+++ libraries/Recaptcha.php
@@ -24,10 +24,13 @@ class Recaptcha {
       "response" => $response,
       "remoteip" => ($remoteip ? $remoteip : getenv("REMOTE_ADDR")),
     ];
+
     $r = Common::curlRequest($this->url, $data);
+
     Logger::logMetric("recaptcha_r_code", $r->code);
     Logger::logMetric("recaptcha_r_type", $r->type);
     Logger::logMetric("recaptcha_r_data", $r->data);
+
     if ($r->code != 200) {
       throw new RecaptchaException("Received bad HTTP status");
     } else if (stripos($r->type, "json") === false) {
@@ -35,12 +38,16 @@ class Recaptcha {
     } else if (empty($data)) {
       throw new RecaptchaException("Received empty response");
     }
+
     $j = json_decode($r->data);
     $e = json_last_error();
+
     Logger::logMetric("recaptcha_json_error", $e);
+
     if (!$j || $e !== JSON_ERROR_NONE || !property_exists($j, "success")) {
       throw new RecaptchaException("Received invalid response");
     }
+
     return ($j->success);
   }
   
