7f542ac4f86196b592568c2e2bfaf99aad26c612
Carl Bennett <carl@carlbennett.me>
2016-08-04 04:32:24 +0000

    Remove unused autoloader code

--- src/main.php
+++ src/main.php
@@ -18,24 +18,6 @@ function main() {
   }
   require(__DIR__ . "/../vendor/autoload.php");
 
-  spl_autoload_register(function($className){
-    $path = $className;
-    if (substr($path, 0, 8) == "BNETDocs") $path = substr($path, 9);
-    $cursor = strpos($path, "\\");
-    if ($cursor !== false) {
-      $path = strtolower(substr($path, 0, $cursor)) . substr($path, $cursor);
-    }
-    $path = str_replace("\\", DIRECTORY_SEPARATOR, $path);
-    $classShortName = $path;
-    $docroot = getenv("DOCUMENT_ROOT");
-    if (empty($docroot)) $docroot = ".";
-    $path = $docroot . DIRECTORY_SEPARATOR . $path . ".php";
-    if (!file_exists($path)) {
-      trigger_error("Class not found: " . $classShortName, E_USER_ERROR);
-    }
-    require_once($path);
-  });
-
   GlobalErrorHandler::createOverrides();
 
   Logger::initialize();
