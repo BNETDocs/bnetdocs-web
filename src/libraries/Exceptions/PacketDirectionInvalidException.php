c7a01fe6f9bfb74f872d1415497b53b73dfa7421
Carl Bennett <carl@carlbennett.me>
2016-08-12 21:01:13 +0000

    Fix copypasta on PacketDirectionInvalidException

--- src/libraries/Exceptions/PacketDirectionInvalidException.php
+++ src/libraries/Exceptions/PacketDirectionInvalidException.php
@@ -6,7 +6,7 @@ use \BNETDocs\Libraries\Exceptions\BNETDocsException;
 use \BNETDocs\Libraries\Logger;
 use \Exception;
 
-class PacketTransportLayerFoundException extends BNETDocsException {
+class PacketDirectionInvalidException extends BNETDocsException {
 
   public function __construct($query, Exception &$prev_ex = null) {
     parent::__construct("Packet direction is invalid", 19, $prev_ex);
