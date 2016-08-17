53d5993e7d3573d99d04dd54c35dc0bb4c2f55f7
Carl Bennett <carl@carlbennett.me>
2016-08-17 05:44:23 +0000

    Add UI for reset password with NYI warning

--- src/models/User/ResetPassword.php
+++ src/models/User/ResetPassword.php
@@ -6,10 +6,18 @@ use \BNETDocs\Libraries\Model;
 
 class ResetPassword extends Model {
 
+  public $csrf_id;
+  public $csrf_token;
+  public $email;
+  public $error;
   public $user_session;
 
   public function __construct() {
     parent::__construct();
+    $this->csrf_id      = null;
+    $this->csrf_token   = null;
+    $this->email        = null;
+    $this->error        = null;
     $this->user_session = null;
   }
 
