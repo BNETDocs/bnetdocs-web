6e3af5a248315f2e94686a23cc462dc36f01e2f7
Carl Bennett <carl@carlbennett.me>
2016-08-24 07:10:13 +0000

    Software License Changes
    The Creative Commons Attribution-NonCommercial 4.0 International
    ("CC BY-NC 4.0") license originally used with the BNETDocs software and
    source code is not valid for software licensing. Since the original
    license is not valid for software licensing, the copyright holder is now
    upgrading the license from its original version.
    
    As of August 24, 2016, the BNETDocs software will be licensed under the
    GNU Affero General Public License v3 ("AGPLv3").
    
    All previously distributed copies and modifications of this software are
    hereby licensed under the AGPLv3 license retroactively.

--- src/main.php
+++ src/main.php
@@ -1,4 +1,22 @@
 <?php
+/**
+ *  BNETDocs, the Battle.net(TM) protocol documentation and discussion website
+ *  Copyright (C) 2008-2016  Carl Bennett
+ *  This file is part of BNETDocs.
+ *
+ *  BNETDocs is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BNETDocs is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with BNETDocs.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace BNETDocs;
 
