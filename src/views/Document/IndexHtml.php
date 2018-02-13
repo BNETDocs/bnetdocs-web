BNETDocs/bnetdocs-web

commit 1ef6e274ccaf072b99cae2c93614bd864f24eed5

Add user index (not paginated)

src/views/Document/IndexHtml.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace BNETDocs\Views\Document;
+namespace BNETDocs\Views\User;
 
-use \BNETDocs\Models\Document\Index as DocumentIndexModel;
+use \BNETDocs\Models\User\Index as UserIndexModel;
 use \CarlBennett\MVC\Libraries\Exceptions\IncorrectModelException;
 use \CarlBennett\MVC\Libraries\Model;
 use \CarlBennett\MVC\Libraries\Template;
@@ -11,14 +11,14 @@ use \CarlBennett\MVC\Libraries\View;
 class IndexHtml extends View {
 
   public function getMimeType() {
-    return "text/html;charset=utf-8";
+    return 'text/html;charset=utf-8';
   }
 
   public function render(Model &$model) {
-    if (!$model instanceof DocumentIndexModel) {
+    if (!$model instanceof UserIndexModel) {
       throw new IncorrectModelException();
     }
-    (new Template($model, "Document/Index"))->render();
+    (new Template($model, 'User/Index'))->render();
   }
 
 }

src/views/User/IndexHtml.php
@@ -3,24 +3,22 @@
 namespace BNETDocs\Views\User;
 
 use \BNETDocs\Models\User\Index as UserIndexModel;
-use \CarlBennett\MVC\Libraries\Common;
 use \CarlBennett\MVC\Libraries\Exceptions\IncorrectModelException;
 use \CarlBennett\MVC\Libraries\Model;
+use \CarlBennett\MVC\Libraries\Template;
 use \CarlBennett\MVC\Libraries\View;
 
-class IndexJSON extends View {
+class IndexHtml extends View {
 
   public function getMimeType() {
-    return 'application/json;charset=utf-8';
+    return "text/html;charset=utf-8";
   }
 
   public function render(Model &$model) {
     if (!$model instanceof UserIndexModel) {
       throw new IncorrectModelException();
     }
-    echo json_encode([
-      'users' => $model->users
-    ], Common::prettyJSONIfBrowser());
+    (new Template($model, "User/Index"))->render();
   }
 
 }

src/views/User/IndexJSON.php
@@ -11,7 +11,7 @@ use \CarlBennett\MVC\Libraries\View;
 class IndexJSON extends View {
 
   public function getMimeType() {
-    return 'application/json;charset=utf-8';
+    return "application/json;charset=utf-8";
   }
 
   public function render(Model &$model) {
@@ -19,7 +19,7 @@ class IndexJSON extends View {
       throw new IncorrectModelException();
     }
     echo json_encode([
-      'users' => $model->users
+      "documents" => $model->documents
     ], Common::prettyJSONIfBrowser());
   }
 

src/controllers/User/Index.php
@@ -1,0 +1,35 @@
+<?php
+
+namespace BNETDocs\Controllers\User;
+
+use \BNETDocs\Libraries\User;
+use \BNETDocs\Models\User\Index as UserIndexModel;
+use \BNETDocs\Views\User\IndexHtml as UserIndexHtmlView;
+use \BNETDocs\Views\User\IndexJSON as UserIndexJSONView;
+use \CarlBennett\MVC\Libraries\Common;
+use \CarlBennett\MVC\Libraries\Controller;
+use \CarlBennett\MVC\Libraries\Gravatar;
+use \CarlBennett\MVC\Libraries\Router;
+use \CarlBennett\MVC\Libraries\View;
+
+class Index extends Controller {
+
+  public function &run(Router &$router, View &$view, array &$args) {
+
+    $model        = new UserIndexModel();
+    $model->users = User::getAllUsers(false);
+
+    // Post-filter summary of users
+    $model->sum_users = count($model->users);
+
+    $view->render($model);
+
+    $model->_responseCode = 200;
+    $model->_responseHeaders['Content-Type'] = $view->getMimeType();
+    $model->_responseTTL = 0;
+
+    return $model;
+
+  }
+
+}

src/libraries/User.php
@@ -239,6 +239,62 @@ class User implements JsonSerializable {
     return ($this->options_bitmask & $acl);
   }
 
+  public static function getAllUsers($reverse) {
+    $cache_key = 'bnetdocs-users';
+    $cache_val = Common::$cache->get($cache_key);
+    if ($cache_val !== false && !empty($cache_val)) {
+      $ids     = explode(',', $cache_val);
+      $objects = [];
+      foreach ($ids as $id) {
+        $objects[] = new self($id);
+      }
+      return $objects;
+    }
+    if (!isset(Common::$database)) {
+      Common::$database = DatabaseDriver::getDatabaseObject();
+    }
+    try {
+      $stmt = Common::$database->prepare("
+        SELECT
+          `created_datetime`,
+          `display_name`,
+          `email`,
+          `id`,
+          `options_bitmask`,
+          `password_hash`,
+          `password_salt`,
+          `timezone`,
+          `username`,
+          `verified_datetime`
+        FROM `users`
+        ORDER BY
+          `created_datetime`
+          " . ($reverse ? 'DESC' : 'ASC') . ",
+          `id`
+          " . ($reverse ? 'DESC' : 'ASC') . "
+        ;
+      ");
+      if (!$stmt->execute()) {
+        throw new QueryException('Cannot refresh all users');
+      }
+      $ids     = [];
+      $objects = [];
+      while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+        $ids[]     = (int) $row->id;
+        $objects[] = new self($row);
+        Common::$cache->set(
+          'bnetdocs-user-' . $row->id, serialize($row), 300
+        );
+      }
+      $stmt->closeCursor();
+      Common::$cache->set($cache_key, implode(',', $ids), 300);
+      return $objects;
+    } catch (PDOException $e) {
+      throw new QueryException('Cannot refresh all users', $e);
+    }
+    return null;
+  }
+
   public function getAvatarURI($size) {
     return Common::relativeUrlToAbsolute(
       (new Gravatar($this->getEmail()))->getUrl($size, "identicon")

src/main.php
@@ -225,6 +225,9 @@ function main() {
       "#^/user/createpassword/?$#",
       "User\\CreatePassword", "User\\CreatePasswordHtml"
     );
+    $router->addRoute( // URL: /user/index
+      "#^/user/index/?$#", "User\\Index", "User\\IndexHtml"
+    );
     $router->addRoute( // URL: /user/login
       "#^/user/login/?$#", "User\\Login", "User\\LoginHtml"
     );

src/models/User/Index.php
@@ -1,0 +1,12 @@
+<?php
+
+namespace BNETDocs\Models\User;
+
+use \CarlBennett\MVC\Libraries\Model;
+
+class Index extends Model {
+
+  public $users;
+  public $sum_users;
+
+}

src/templates/User/Index.phtml
@@ -1,0 +1,37 @@
+<?php
+
+namespace BNETDocs\Templates\User;
+
+use \BNETDocs\Libraries\User;
+use \CarlBennett\MVC\Libraries\Common;
+use \CarlBennett\MVC\Libraries\Pair;
+
+$title       = 'User Index';
+$description = 'The index for users on BNETDocs';
+$this->opengraph->attach(new Pair('url', '/user/index'));
+
+require('./header.inc.phtml');
+?>
+      <article>
+        <header>User Index</header>
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th class="left">Found <?php echo number_format($this->getContext()->sum_users); ?> user<?php echo ($this->getContext()->sum_users != 1 ? 's' : ''); ?></th>
+                <th class="left">Registered</th>
+              </tr>
+            </thead><tbody>
+<?php foreach ($this->getContext()->users as $user) {
+        $user_url   = $user->getURI();
+        $avatar_url = $user->getAvatarURI(22);
+?>
+              <tr>
+                <td><a href="<?php echo $user_url; ?>"><img class="avatar" src="<?php echo $avatar_url; ?>"/> <?php echo filter_var($user->getName(), FILTER_SANITIZE_FULL_SPECIAL_CHARS); ?></a></td>
+                <td><time datetime="<?php echo $user->getCreatedDateTime()->format('c'); ?>"><?php echo $user->getCreatedDateTime()->format('l, F j, Y'); ?></time></td>
+              </tr>
+<?php } ?>
+          </tbody></table>
+        </section>
+      </article>
+<?php require("./footer.inc.phtml"); ?>

commit 89531e4279c7c5d9a4da5c424fb2204f5df943f5

Fix bug introduced in 1ef6e27

src/views/Document/IndexHtml.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BNETDocs\Views\User;
+namespace BNETDocs\Views\Document;
 
 use \BNETDocs\Models\User\Index as UserIndexModel;
 use \CarlBennett\MVC\Libraries\Exceptions\IncorrectModelException;

src/views/Document/IndexJSON.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace BNETDocs\Views\User;
+namespace BNETDocs\Views\Document;
 
 use \BNETDocs\Models\User\Index as UserIndexModel;
 use \CarlBennett\MVC\Libraries\Common;