bcf32852f0742fb0c076923730ee41279f18db9a
Carl Bennett <carl@carlbennett.me>
2018-02-26 02:57:17 +0000

    Display better version info on non-production

--- src/libraries/VersionInfo.php
+++ src/libraries/VersionInfo.php
@@ -6,6 +6,8 @@ use \StdClass;
 
 class VersionInfo {
 
+  const VERSION_INFO_FILE = '../etc/.rsync-version';
+
   public static $version;
 
   /**
@@ -14,11 +16,30 @@ class VersionInfo {
   private function __construct() {}
 
   public static function get() {
-    $versions           = new StdClass();
-    $versions->bnetdocs = file_get_contents('../etc/.rsync-version');
-    $versions->bnetdocs = explode("\n", $versions->bnetdocs);
+    $versions = new StdClass();
+
+    $versions->bnetdocs = self::getBNETDocsVersion();
     $versions->php      = phpversion();
+
     return $versions;
+  }
+
+  private static function getBNETDocsVersion() {
+    if ( !file_exists( self::VERSION_INFO_FILE ) ) {
+      return null;
+    }
+
+    $buffer = file_get_contents( self::VERSION_INFO_FILE );
+
+    if ( empty( $buffer ) ) {
+      return null;
+    }
+
+    // The deploy server generates the file with "\n",
+    // so don't use PHP_EOL here.
+    $buffer = explode( "\n", $buffer );
+
+    return $buffer;
   }
 
 }
