66e70e5e7198b5d87d517d1d80d6a606384b78e3
Carl Bennett <carl@carlbennett.me>
2019-12-06 20:10:44 +0000

    Fix E_NOTICE on nullable event log fields

--- src/libraries/Logger.php
+++ src/libraries/Logger.php
@@ -22,8 +22,11 @@ class Logger extends LoggerMVCLib {
     if (!isset(Common::$database)) {
       Common::$database = DatabaseDriver::getDatabaseObject();
     }
+
     $successful = false;
+
     try {
+
       $stmt = Common::$database->prepare('
         INSERT INTO `event_log` (
           `event_type_id`, `event_datetime`, `user_id`, `ip_address`,
@@ -32,14 +35,24 @@ class Logger extends LoggerMVCLib {
           :event_type_id, NOW(), :user_id, :ip_address, :meta_data
         );
       ');
+
       $stmt->bindParam(':event_type_id', $event_type_id, PDO::PARAM_INT);
-      $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-      $stmt->bindParam(':ip_address', $ip_address, PDO::PARAM_STR);
-      $stmt->bindParam(':meta_data', $meta_data, PDO::PARAM_STR);
+
+      $t = (is_null($user_id) ? PDO::PARAM_NULL : PDO::PARAM_INT);
+      $stmt->bindParam(':user_id', $user_id, $t);
+
+      $t = (is_null($ip_address) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+      $stmt->bindParam(':ip_address', $ip_address, $t);
+
+      $t = (is_null($meta_data) ? PDO::PARAM_NULL : PDO::PARAM_STR);
+      $stmt->bindParam(':meta_data', $meta_data, $t);
+
       $successful = $stmt->execute();
       $stmt->closeCursor();
+
     } catch (PDOException $e) {
       throw new QueryException('Cannot log event', $e);
+
     } finally {
       return $successful;
     }
