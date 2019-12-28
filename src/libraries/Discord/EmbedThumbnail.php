fe98ed88e3072c1b617a5210225dbbbdc4bcd07a
Carl Bennett <carl@carlbennett.me>
2019-12-28 15:20:53 +0000

    Remove unused class

--- src/libraries/Discord/EmbedThumbnail.php
+++ src/libraries/Discord/EmbedThumbnail.php
@@ -3,7 +3,6 @@
 namespace BNETDocs\Libraries\Discord;
 
 use \BNETDocs\Libraries\Discord\EmbedImage;
-use \JsonSerializable;
 
 // <https://discordapp.com/developers/docs/resources/channel#embed-object-embed-thumbnail-structure>
 
