73ad3deac0475cad98194a77f95fc6016d21f4d0
Carl Bennett <carl@carlbennett.me>
2021-06-24 19:07:49 -0500

    Add code block styling

--- src/templates/MarkdownBootstrapFix.inc.php
+++ src/templates/MarkdownBootstrapFix.inc.php
@@ -6,9 +6,17 @@ namespace BNETDocs\Templates;
  */
 function MarkdownBootstrapFix(string $v)
 {
+  // Tables
   $v = str_replace('<table>', '<table class="table table-hover table-markdown table-striped">', $v);
+
+  // Blockquotes
   $v = str_replace('<blockquote>', '<blockquote class="blockquote">', $v);
+
+  // Images
   $v = str_replace('<img ', '<img class="img-fluid" ', $v);
+
+  // Code Blocks
+  $v = str_replace('<pre><code>', '<pre class="border border-primary p-2 pre-scrollable rounded text-light" style="overflow-y:auto;"><code>', $v);
 
   return $v;
 }
