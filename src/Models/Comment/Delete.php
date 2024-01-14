492a8ef4606c46e219aa8ee25b4fe14a81446be0
Carl Bennett <carl@carlbennett.me>
2024-01-14 11:16:44 -0600

    Fix deprecation notice on content variable

--- src/Models/Comment/Delete.php
+++ src/Models/Comment/Delete.php
@@ -6,6 +6,7 @@ class Delete extends \BNETDocs\Models\ActiveUser
 {
   public bool $acl_allowed = false;
   public ?\BNETDocs\Libraries\Comment $comment = null;
+  public ?string $content = null;
   public ?int $id = null;
   public ?int $parent_id = null;
   public ?int $parent_type = null;
