ae22cf3030080f9b2b9b83327d706bb9c3e91882
Carl Bennett <carl@carlbennett.me>
2025-05-20 08:10:11 -0500

    Adjust ordering of content in search results

--- src/Libraries/Search/Search.php
+++ src/Libraries/Search/Search.php
@@ -29,7 +29,8 @@ class Search
         self::runIdQuery($pdo, $results,
             'SELECT `id` FROM `comments` WHERE
                 MATCH(`content`) AGAINST (:fulltext IN BOOLEAN MODE)
-                OR `content` LIKE :like',
+                OR `content` LIKE :like
+                ORDER BY `created_datetime` DESC, `id` DESC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new Comment((int) $id),
             fn($r, $obj) => $r->addComment($obj)
@@ -41,7 +42,8 @@ class Search
                 MATCH(`title`, `brief`, `content`) AGAINST (:fulltext IN BOOLEAN MODE)
                 OR `title` LIKE :like
                 OR `brief` LIKE :like
-                OR `content` LIKE :like',
+                OR `content` LIKE :like
+                ORDER BY IFNULL(`edited_datetime`, `created_datetime`) DESC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new Document((int) $id),
             fn($r, $obj) => $r->addDocument($obj)
@@ -52,7 +54,8 @@ class Search
             'SELECT `id` FROM `news_posts` WHERE
                 MATCH(`title`, `content`) AGAINST (:fulltext IN BOOLEAN MODE)
                 OR `title` LIKE :like
-                OR `content` LIKE :like',
+                OR `content` LIKE :like
+                ORDER BY IFNULL(`edited_datetime`, `created_datetime`) DESC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new NewsPost((int) $id),
             fn($r, $obj) => $r->addNewsPost($obj)
@@ -66,7 +69,8 @@ class Search
                 OR `packet_name` LIKE :like
                 OR `packet_brief` LIKE :like
                 OR `packet_format` LIKE :like
-                OR `packet_remarks` LIKE :like',
+                OR `packet_remarks` LIKE :like
+                ORDER BY `packet_id` ASC, `packet_name` ASC, `packet_direction_id` ASC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new Packet((int) $id),
             fn($r, $obj) => $r->addPacket($obj)
@@ -77,7 +81,8 @@ class Search
             'SELECT `id` FROM `servers` WHERE
                 MATCH(`label`, `address`) AGAINST (:fulltext IN BOOLEAN MODE)
                 OR `label` LIKE :like
-                OR `address` LIKE :like',
+                OR `address` LIKE :like
+                ORDER BY `label` ASC, `address` ASC, `port` ASC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new Server((int) $id),
             fn($r, $obj) => $r->addServer($obj)
@@ -88,7 +93,8 @@ class Search
             'SELECT `id` FROM `users` WHERE
                 MATCH(`username`, `display_name`) AGAINST (:fulltext IN BOOLEAN MODE)
                 OR `username` LIKE :like
-                OR `display_name` LIKE :like',
+                OR `display_name` LIKE :like
+                ORDER BY IFNULL(`display_name`, `username`) ASC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new User((int) $id),
             fn($r, $obj) => $r->addUser($obj)
@@ -117,7 +123,8 @@ class Search
                 OR `facebook_username` LIKE :like
                 OR `twitter_username` LIKE :like
                 OR `instagram_username` LIKE :like
-                OR `skype_username` LIKE :like',
+                OR `skype_username` LIKE :like
+                ORDER BY `user_id` ASC',
             [':fulltext' => $fulltext, ':like' => $like],
             fn($id) => new User((int) $id),
             fn($r, $obj) => $r->addUser($obj)
