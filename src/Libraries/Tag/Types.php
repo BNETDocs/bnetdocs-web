b9f9e8d0feb7a32b249b477b1ec09475752b5420
Carl Bennett <carl@carlbennett.me>
2025-05-22 03:39:33 -0500

    Swap tag type ids

--- src/Libraries/Tag/Types.php
+++ src/Libraries/Tag/Types.php
@@ -4,8 +4,8 @@ namespace BNETDocs\Libraries\Tag;
 
 enum Types: int
 {
-    case Document = 0;
-    case Comment = 1;
+    case Comment = 0;
+    case Document = 1;
     case NewsPost = 2;
     case Packet = 3;
     case Server = 4;
@@ -14,8 +14,8 @@ enum Types: int
     public static function fromInt(int $value): self
     {
         return match($value) {
-            0 => self::Document,
-            1 => self::Comment,
+            0 => self::Comment,
+            1 => self::Document,
             2 => self::NewsPost,
             3 => self::Packet,
             4 => self::Server,
