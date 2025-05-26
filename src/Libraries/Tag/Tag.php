ae28e296d47496982f53584389a5652537865641
Carl Bennett <carl@carlbennett.me>
2025-05-25 20:08:34 -0500

    Add created datetime property to Tag class

--- src/Libraries/Tag/Tag.php
+++ src/Libraries/Tag/Tag.php
@@ -4,6 +4,7 @@ namespace BNETDocs\Libraries\Tag;
 
 use \BNETDocs\Libraries\Db\MariaDb;
 use \BNETDocs\Libraries\Tag\Types;
+use \DateTimeInterface;
 use \OutOfBoundsException;
 
 class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
@@ -11,6 +12,7 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
     public const MAX_REFERENCE_ID = 0x7FFFFFFFFFFFFFFF;
     public const MAX_REFERENCE_TYPE = 0x7FFFFFFFFFFFFFFF;
 
+    private ?DateTimeInterface $created_datetime = null;
     private ?int $reference_id = null;
     private ?Types $reference_type = null;
     private ?string $tag_string = null;
@@ -43,7 +45,8 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
                 SELECT
                     `reference_id`,
                     `reference_type`,
-                    `tag_string`
+                    `tag_string`,
+                    `created_datetime`
                 FROM `tags` WHERE
                     `reference_id` = :refid AND
                     `reference_type` = :reftype AND
@@ -74,7 +77,8 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
                 SELECT
                     `reference_id`,
                     `reference_type`,
-                    `tag_string`
+                    `tag_string`,
+                    `created_datetime`
                 FROM `tags` WHERE
                     `reference_id` = :refid AND
                     `reference_type` = :reftype
@@ -96,16 +100,29 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
         $this->setReferenceId($value->reference_id ?? null);
         $this->setReferenceType($value->reference_type ?? null);
         $this->setTagString($value->tag_string ?? null);
+        $this->setCreatedDateTime($value->created_datetime ?? null);
     }
 
     public function commit(): bool
     {
         $p = [
+            'created' => $this->getCreatedDateTime(),
             'refid' => $this->getReferenceId(),
             'reftype' => $this->getReferenceType(),
             'tagstr' => $this->getTagString(),
         ];
-        if ($p['reftype'] instanceof Types) $p['reftype'] = $p['reftype']->toInt();
+
+        foreach ($p as $k => $v)
+        {
+            if ($v instanceof DateTimeInterface)
+            {
+                $p[$k] = $v->format(self::DATE_SQL);
+            }
+            else if ($v instanceof Types)
+            {
+                $p[$k] = $v->toInt();
+            }
+        }
 
         try
         {
@@ -113,13 +130,16 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
                 INSERT INTO `tags` (
                     `reference_id`,
                     `reference_type`,
-                    `tag_string`
+                    `tag_string`,
+                    `created_datetime`
                 ) VALUES (
-                    `reference_id` = :refid,
-                    `reference_type` = :reftype,
-                    `tag_string` = :tagstr
+                    :refid,
+                    :reftype,
+                    :tagstr,
+                    :created
                 ) ON DUPLICATE KEY UPDATE
-                    `tag_string` = :tagstr;
+                    `tag_string` = :tagstr,
+                    `created_datetime` = :created;
             ');
 
             if ($q && $q->execute($p))
@@ -167,6 +187,11 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
         }
     }
 
+    public function getCreatedDateTime(): ?DateTimeInterface
+    {
+        return $this->created_datetime;
+    }
+
     public function getReferenceId(): ?int
     {
         return $this->reference_id;
@@ -185,10 +210,18 @@ class Tag implements \BNETDocs\Interfaces\DatabaseObject, \JsonSerializable
     public function jsonSerialize(): mixed
     {
         return [
+            'created_datetime' => $this->getCreatedDateTime(),
             'reference_id' => $this->getReferenceId(),
             'reference_type' => $this->getReferenceType(),
             'tag_string' => $this->getTagString(),
         ];
+    }
+
+    public function setCreatedDateTime(DateTimeInterface|string|null $value): void
+    {
+        $this->created_datetime = (is_string($value) ?
+            new \DateTimeImmutable($value, new \DateTimeZone(self::DATE_TZ)) : $value
+        );
     }
 
     public function setReferenceId(?int $value): void
